65c9a3aa8de22f895ca2e3b5817055723cabb637
Administrator <godxuche@gmail.com>
2021-10-27 07:27:30 +0100

    implemented work chaining and running parallel work chains by combining the WorkContinuations...

--- app/src/main/java/com/example/workchains/MainActivity.kt
+++ app/src/main/java/com/example/workchains/MainActivity.kt
@@ -3,10 +3,17 @@ package com.example.workchains
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkContinuation
+import androidx.work.WorkManager
+import androidx.work.workDataOf
 import com.example.workchains.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val workManager = WorkManager.getInstance(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -19,5 +26,144 @@ class MainActivity : AppCompatActivity() {
         val btnGroupChainFail = binding.btnGroupChainFail
         val btnMultipleChainSucceed = binding.btnMultipleChainSucceed
         val btnMultipleChainFail = binding.btnMultipleChainFail
+
+        btnSingleChainSucceed.setOnClickListener {
+            val objectDetectionWorkerRequest = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true))
+                .build()
+            val networkRequestWorkerRequest = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true))
+                .build()
+            val databaseWriterWorkerRequest = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true))
+                .build()
+
+            workManager.beginWith(objectDetectionWorkerRequest)
+                .then(networkRequestWorkerRequest)
+                .then(databaseWriterWorkerRequest)
+                .enqueue()
+        }
+
+        btnSingleChainFail.setOnClickListener {
+            val objectDetectionWorkerRequest = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true))
+                .build()
+            val networkRequestWorkerRequest = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to false))
+                .build()
+            val databaseWriterWorkerRequest = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true))
+                .build()
+
+            workManager.beginWith(objectDetectionWorkerRequest)
+                .then(networkRequestWorkerRequest)
+                .then(databaseWriterWorkerRequest)
+                .enqueue()
+        }
+
+        btnGroupChainSucceed.setOnClickListener {
+            val objectDetectionWorkerRequest1 = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true))
+                .build()
+            val objectDetectionWorkerRequest2 = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true))
+                .build()
+            val networkRequestWorkerRequest = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true))
+                .build()
+            val databaseWriterWorkerRequest = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true))
+                .build()
+
+            workManager.beginWith(listOf(objectDetectionWorkerRequest1, objectDetectionWorkerRequest2))
+                .then(networkRequestWorkerRequest)
+                .then(databaseWriterWorkerRequest)
+                .enqueue()
+        }
+
+        btnGroupChainFail.setOnClickListener {
+            val objectDetectionWorkerRequest1 = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true))
+                .build()
+            val objectDetectionWorkerRequest2 = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to false))
+                .build()
+            val networkRequestWorkerRequest = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true))
+                .build()
+            val databaseWriterWorkerRequest = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true))
+                .build()
+
+
+            workManager.beginWith(listOf(objectDetectionWorkerRequest1, objectDetectionWorkerRequest2))
+                .then(networkRequestWorkerRequest)
+                .then(databaseWriterWorkerRequest)
+                .enqueue()
+        }
+
+        btnMultipleChainSucceed.setOnClickListener {
+            val objectDetectionWorkerRequest1 = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true, "NAME" to "ONE"))
+                .build()
+            val objectDetectionWorkerRequest2 = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true, "NAME" to "TWO"))
+                .build()
+            val networkRequestWorkerRequest1 = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true, "NAME" to "ONE"))
+                .build()
+            val networkRequestWorkerRequest2 = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true, "NAME" to "TWO"))
+                .build()
+            val databaseWriterWorkerRequest1 = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true, "NAME" to "ONE"))
+                .build()
+            val databaseWriterWorkerRequest2 = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true, "NAME" to "TWO"))
+                .build()
+
+            val recommendation1 = workManager.beginWith(objectDetectionWorkerRequest1)
+                .then(networkRequestWorkerRequest1)
+                .then(databaseWriterWorkerRequest1)
+
+            val recommendation2 = workManager.beginWith(objectDetectionWorkerRequest2)
+                .then(networkRequestWorkerRequest2)
+                .then(databaseWriterWorkerRequest2)
+
+            val root = WorkContinuation.combine(listOf(recommendation1, recommendation2))
+            root.enqueue()
+        }
+
+        btnMultipleChainFail.setOnClickListener {
+            val objectDetectionWorkerRequest1 = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true, "NAME" to "ONE"))
+                .build()
+            val objectDetectionWorkerRequest2 = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true, "NAME" to "TWO"))
+                .build()
+            val networkRequestWorkerRequest1 = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true, "NAME" to "ONE"))
+                .build()
+            val networkRequestWorkerRequest2 = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to false, "NAME" to "TWO"))
+                .build()
+            val databaseWriterWorkerRequest1 = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true, "NAME" to "ONE"))
+                .build()
+            val databaseWriterWorkerRequest2 = OneTimeWorkRequestBuilder<ObjectDetectionWorker>()
+                .setInputData(workDataOf("SUCCESS" to true, "NAME" to "TWO"))
+                .build()
+
+            val recommendation1 = workManager.beginWith(objectDetectionWorkerRequest1)
+                .then(networkRequestWorkerRequest1)
+                .then(databaseWriterWorkerRequest1)
+
+            val recommendation2 = workManager.beginWith(objectDetectionWorkerRequest2)
+                .then(networkRequestWorkerRequest2)
+                .then(databaseWriterWorkerRequest2)
+
+            val root = WorkContinuation.combine(listOf(recommendation1, recommendation2))
+            root.enqueue()
+        }
     }
 }
---
+++ app/src/main/java/com/example/workchains/ObjectDetectionWorker.kt
@@ -0,0 +1,25 @@
+package com.example.workchains
+
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import kotlin.random.Random
+
+class ObjectDetectionWorker(context: Context, params: WorkerParameters): Worker(context, params) {
+    override fun doWork(): Result {
+        Thread.sleep(1000)
+        println({this::class.java.simpleName})
+
+        val success = inputData.getBoolean("SUCCESS", false)
+        val name = inputData.getString("NAME")
+        return if (success) {
+            name?.let { println("$name success") }
+            val (color, product) = ProductDatabase.products[Random.nextInt(0, ProductDatabase.products.size)]
+            println("Detected $color $product")
+            Result.success()
+        } else {
+            name?.let { println("$name failure") }
+            Result.failure()
+        }
+    }
+}
